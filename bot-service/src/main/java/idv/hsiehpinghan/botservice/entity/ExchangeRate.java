d268fc48658ba6dfc560fc70c8718a4d4b6a57b3
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2014-12-31 17:20:57 +0800

    Backup.

---
+++ bot-service/src/main/java/idv/hsiehpinghan/botservice/entity/ExchangeRate.java
@@ -0,0 +1,99 @@
+package idv.hsiehpinghan.botservice.entity;
+
+import idv.hsiehpinghan.botservice.enumeration.Dollar;
+import idv.hsiehpinghan.datatypeutility.utility.ArrayUtility;
+import idv.hsiehpinghan.datatypeutility.utility.IntegerUtility;
+import idv.hsiehpinghan.datatypeutility.utility.LongUtility;
+import idv.hsiehpinghan.hbaseassistant.abstractclass.HBaseColumnFamily;
+import idv.hsiehpinghan.hbaseassistant.abstractclass.HBaseColumnQualifier;
+import idv.hsiehpinghan.hbaseassistant.abstractclass.HBaseRowKey;
+import idv.hsiehpinghan.hbaseassistant.abstractclass.HBaseTable;
+import idv.hsiehpinghan.hbaseassistant.abstractclass.HBaseValue;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.hadoop.hbase.util.Bytes;
+
+/**
+ * Exchange rate table.
+ * 
+ * @author thank.hsiehpinghan
+ *
+ */
+public class ExchangeRate extends HBaseTable {
+	
+	public ExchangeRate() {
+		super();
+	}
+	public ExchangeRate(HBaseRowKey key) {
+		super(key);
+	}
+	
+	public class Key extends HBaseRowKey {
+		private final int DOLLAR_BEGIN = 0;
+		private final int DOLLAR_END = DOLLAR_BEGIN + 3;
+		private final int DATE_BEGIN = DOLLAR_END;
+		private final int DATE_END = DATE_BEGIN + LongUtility.LONG_BYTE_AMOUNT;
+		private Dollar dollar;
+		private Date date;
+
+		public Key() {
+			super();
+		}
+
+		public Key(Dollar dollar, Date date) {
+			super();
+			this.dollar = dollar;
+			this.date = date;
+		}
+		
+		@Override
+		public byte[] toBytes() {
+			byte[] dollarArr = Bytes.toBytes(dollar.name());
+			byte[] dateArr = Bytes.toBytes(date.getTime());
+			byte[] all = ArrayUtility.addAll(dollarArr, dateArr);
+			return all;
+		}
+
+		@Override
+		public void fromBytes(byte[] bytes) {
+			String dollarStr = new String(ArrayUtils.subarray(bytes,
+					DOLLAR_BEGIN, DOLLAR_END));
+			this.dollar = Dollar.valueOf(dollarStr);
+			long time = Bytes.toLong(ArrayUtils.subarray(bytes, DATE_BEGIN,
+					DATE_END));
+			this.date = new Date(time);
+		}
+
+		public Dollar getDollar() {
+			return dollar;
+		}
+
+		public void setDollar(Dollar dollar) {
+			this.dollar = dollar;
+		}
+
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
+		}
+
+		@Override
+		public String toString() {
+			return "Key [dollar=" + dollar + ", date=" + date + "]";
+		}
+
+		
+	}
+
+}
--- bot-service/src/main/java/idv/hsiehpinghan/botservice/manager/hbase/BotHbaseManager.java
+++ bot-service/src/main/java/idv/hsiehpinghan/botservice/manager/hbase/BotHbaseManager.java
@@ -1,60 +1,38 @@
 package idv.hsiehpinghan.botservice.manager.hbase;
 
+import idv.hsiehpinghan.botservice.enumeration.Dollar;
 import idv.hsiehpinghan.botservice.manager.IBotManager;
+import idv.hsiehpinghan.botservice.operator.ExchangeRateDownloader;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BotHbaseManager implements IBotManager {
-	private Logger logger = Logger.getLogger(this.getClass().getName());
-	
+public class BotHbaseManager implements IBotManager, InitializingBean {
+//	private Logger logger = Logger.getLogger(this.getClass().getName());
+	private List<Dollar> targetDallars;
+	@Autowired
+	private ExchangeRateDownloader downloader;
 
 	public File downloadExchangeRate() {
-		// TODO Auto-generated method stub
-		return null;
+		return downloader.downloadExchangeRate(targetDallars);
 	}
 
 	public boolean saveExchangeRateToDatabase(File dataDirectory) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	//
-	// @Autowired
-	// private FinancialReportDownloader downloader;
-	//
-	// @Override
-	// public File downloadFinancialReport() {
-	// try {
-	// File xbrlDir = downloader.downloadFinancialReport();
-	// logger.info(xbrlDir.getAbsolutePath() + " download finish.");
-	// return xbrlDir;
-	// } catch (Exception e) {
-	// logger.error("Download fail !!!");
-	// return null;
-	// }
-	// }
-	//
-	// @Override
-	// public boolean saveFinancialReportToDatabase(File xbrlDirectory) {
-	// processSubFiles(xbrlDirectory);
-	// return false;
-	// }
-	//
-	// private void processSubFiles(File file) {
-	// if (file.isDirectory()) {
-	// File[] fs = file.listFiles();
-	// for (File f : fs) {
-	// processSubFiles(f);
-	// }
-	// } else {
-	// // xbrlAssistant.get
-	// System.err.println(file.getAbsolutePath());
-	// }
-	//
-	// }
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		targetDallars = new ArrayList<Dollar>(1);
+		targetDallars.add(Dollar.USD);
+	}
 
 }
